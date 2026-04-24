0c2a9e1e11e016e2382f693efba0bc196250067c
ashirafnewton-ait <ashirafnewton-ait@github.com>
2026-04-24 13:20:59 +0000

    Fix missing onAudioData callback in VAD start

--- sandbox/sandbox-main.js
+++ sandbox/sandbox-main.js
@@ -86,6 +86,10 @@ async function startRESTMode() {
                         onTTS: handleTTS,
                         onLog: sendLog
                     });
+                },
+                onAudioData: (data, isTalking) => {
+                    // Required callback - VAD needs this
+                    // Energy visualization could go here
                 }
             });
         } else {
